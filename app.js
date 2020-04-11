d5d9674cb06ec66983b03c3b0477d642b1c81458
edimdendidiwangga <edimdendy@gmail.com>
2020-04-11 17:38:34 +0700

    12. Adding a 404 Error Page

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(adminRouter); 
-
 app.use(shopRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>')
+})
 
 app.listen(3000);
