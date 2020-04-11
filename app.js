a86dc3912213765fb877100ec7835a1ec42e2f18
edimdendidiwangga <edimdendy@gmail.com>
2020-04-11 15:43:41 +0700

    3. Installing Express.js

--- app.js
+++ app.js
@@ -1,42 +1,8 @@
 const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk)
-        })
-        return req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1]
-            console.log(message)
-            fs.writeFile('message.txt', message, () => {
-                res.statusCode = 302
-                res.setHeader('Location', '/')
-                res.end()
-            })
-        })
-        
-    }
+const app = express();
 
-    
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>my first page</title></head>');
-    res.write('<body><h1>Welcome form my node js</h1></body>');
-    res.write('</html>');
-    res.end();
-
-});
+const server = http.createServer(app);
 
 server.listen(3000);
