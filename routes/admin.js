849177eabf5dbd84036f74d15d1c4f8267ce785e
edimdendidiwangga <edimdendy@gmail.com>
2020-04-12 06:09:09 +0700

    18. Styling our Pages

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('../utils/path')
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path('views', 'add.html'))
+    res.sendFile(path('views', 'add-product.html'))
 });
 
 router.post('/add-product', (req, res, next) => {
