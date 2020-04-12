5870c93470566446f058399cfbdf273e59095d73
edimdendidiwangga <edimdendy@gmail.com>
2020-04-13 05:46:53 +0700

    product detail

--- models/product.js
+++ models/product.js
@@ -41,7 +41,7 @@ module.exports = class Product {
 
   static findById(id, cb) {
     getProductsFromFile(products => {
-      const product = products.find(p => p.idid === id)
+      const product = products.find(p => p.id === id)
       cb(product)
     });
   }
